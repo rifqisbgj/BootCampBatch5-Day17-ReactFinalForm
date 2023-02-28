33fd9db1fd668cd331cb4a5e19955d1f7ab90fa7
Rifqi Subagja <rifqisubagjaa@gmail.com>
2023-02-28 12:28:10 +0700

    react final form

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./2802-FinalFormReact/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// memanggil comp App
+root.render(<App />);
